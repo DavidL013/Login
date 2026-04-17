6c447b9c1caf9377b8ad58c7a76763f8cfec9fe6
David Lucas <136875251+DavidL013@users.noreply.github.com>
2026-04-16 21:11:28 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const mensagem = document.querySelector(".mensagem");
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if(email.value === 'admin@email.com' && senha.value === '1234') {
+        alert('Login bem-sucedido!');
+    } else {
+        alert('Email ou senha incorretos!');
+    }
+})
